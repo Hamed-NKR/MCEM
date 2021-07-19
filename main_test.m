b8af24fe4b96c8890b3ae2ea54fe3270ad454b8b
Hamed <hmdnikookar@gmail.com>
2021-07-19 15:34:22 -0700

    Minor initialization updates

--- main_test.m
+++ main_test.m
@@ -1,94 +1,18 @@
 % "main_test" is a script to examine and monitor the performance and...
-    % ...outputs of "MCEM" program.
+%   ...outputs of "MCEM" program.
 
 clc
 clear
 clf('reset')
 close all
 
-%% Part 1.1: Defining the constant physical parameters of the problem
+%% Part 1. Setting the initial domain conditions
 
-% Creating the table of constant parameters
-Name = {'rho_bc'; 'M_air'; 'kb'; 'Na'; 'Ru'};
-Value = [1.8e3; 28.97e-3; 1.381e-23; 6.022e23; 8.314];
-Unit = {'kg/m3'; 'kg/mol'; 'j/k'; 'mol^-1'; 'j/mol.k'};
-Description = {'Black Carbon bulk density'; 'Air molar mass';...
-    'Boltzmann constant'; 'Avogadro constant'; 'Universal gas constant'};
-params_const = table(Name, Value, Unit, Description);
+[params_ud, params_const] = TRANSP.INIT_PARAMS(); % Initializing the...
+    % ...physical parameters to be used in the simulations
 
-clear Name Value Unit Description % Cleaning the worksapce
-
-%% Part 1.2: Importing the user-defined parameters of domain, particle and flow
-
-% Extracting data from the input file
-f_id = fopen('inputs\MCEM_Params.txt','r');
-import_data = textscan(f_id,'%s%f%s%s','Headerlines',2,...
-    'Delimiter','\t','MultipleDelimsAsOne',1);
-fclose(f_id);
-
-% Creating the table of user-defined parameters
-Name = import_data{1,1};
-Value = import_data{1,2};
-Unit = import_data{1,3};
-Description = import_data{1,4};
-params_ud = table(Name, Value, Unit, Description);
-% NOTE: See the "Description" column for more info on these parameters.
-
-clear file_id import_data ans Name Value Unit Description % Cleaning the...
-    % ...worksapce
-
-%% Part 1.3: Initializing the fluid and particle transport variables
-
-% Declaring the fluid structure and loading the corresponding imported...
-    % ...properties
-fl = struct('temp', params_ud.Value(10), 'v', params_ud.Value(11:3),...
-    'p', params_ud.Value(14), 'mu', [],'lambda', []);
-% "fl" is a structure containing the main physical properties of the...
-    % ...background fluid. It is used to consider and study the...
-    % ...particle-fluid interactions.
-% Fields:
-    % temp: Fluid temperature
-    % v: ~ velocity
-    % p: ~ pressure
-    % mu: ~ viscosity
-    % lambda: ~ mean free path
-
-[fl.mu, fl.lambda] = TRANSP.FLPROPS(fl,params_const); % Calculating the...
-    % ...fluid viscosity and mean free path
-
-% Declaring the particle structure
-pars = struct('pp', [], 'n', [], 'dv', [], 'dg', [], 'dm', [], 'da', [],...
-    'dmax', [], 'dpp', [], 'r', [], 'v', [], 'm', [], 'rho', [],...
-     'delt', [], 'tau', [], 'f', [], 'diff', [], 'lambda', [],...
-     'kn_kin', [], 'kn_diff', [], 'nnl', []);
-% "pars" is a structure conating the main physical properties of...
-    % ...independent particles (whether being single monomers or...
-    % ...aggregates) as well as the properties of their constituent...
-    % ...primary particles (if applicable).
-% Fields:
-    % pp: Primary particle characteristics (an N*5 matrix containing...
-        % ...their index, size, and location)
-    % n: Number of primaries within the particles
-    % d_v: Particles equivalent volumetric diameter
-    % d_g: ~ gyration diameter
-    % d_m: ~ mobility diameter
-    % d_a: ~ aerodynamic diameter
-    % d_max: ~ maximum extent diameter
-    % d_pp: ~ mean + std of diameter of primaries
-    % r: ~ spatial location
-    % v: ~ velocity
-    % m: ~ mass
-    % rho: ~ effective density
-    % delt: ~ motion time-step
-    % tau: ~ relaxation time
-    % f: ~ friction factor
-    % diff: ~ diffusivity
-    % lambda: ~ diffusive mean free path
-    % kn_kin: Kinetic Knudsen number
-    % kn_diff: Diffusive Knudsen number
-    % nnl: ~ nearest neighbor list
-% NOTE: The rows of each field correspond to characteristics of each...
-    % ...independent particle.
+[pars, fl] = TRANSP.INIT_DOM(params_ud, params_const); % Initializing...
+    % ...the fluid and particle transport variables
 
 % Calculating the primary particle size and number distributions
 [pp_d, pars.n] = PAR.INIT_DIAM(params_ud.Value(4), params_ud.Value(5:6),...
@@ -126,7 +50,7 @@ disp("The computational domain is successfully initialized...")
 % Visualizing the initial particle locations and velocities, and nearest...
     % ...neighbor lists
 figure
-h_init = UTILS.PLOTPARS(pars, params_ud.Value(1:3),...
+h0_pose = UTILS.PLOTPARS(pars, params_ud.Value(1:3),...
     'equivalent_volumetric_size', 'on', 'velocity_vector', 'on',...
     'render', 'on');
 
@@ -134,11 +58,11 @@ figure
 ind_trg_test = (randperm(params_ud.Value(4),...
     min([params_ud.Value(4), 2])))'; % A random portion of target...
         ...indices for nearest neighbor testing
-h_nntest = UTILS.PLOTNN(pars, params_ud.Value(1:3), ind_trg_test,...
+h0_nntest = UTILS.PLOTNN(pars, params_ud.Value(1:3), ind_trg_test,...
     coef_trg(ind_trg_test));
 
 figure
-h_3d = UTILS.RENDER(pars);
+h0_3d = UTILS.RENDER(pars);
 
 %% Part 2: Simulating the particle aggregations
 
@@ -225,3 +149,5 @@ end
 % Obtaining fractal properties
 [df, kf] = PAR.FRACTALITY(pars);
 
+figure
+h_3d = UTILS.RENDER(pars); % Morphology of the final population
