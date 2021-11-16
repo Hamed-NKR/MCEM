32aa3240646513443cfa3648512ff74b2e0d2f49
Hamed Nikookar <70616948+Hamed-NKR@users.noreply.github.com>
2021-11-16 12:45:10 -0800

    Plot fractality update

--- +UTILS/PLOTFRACTALITY.m
+++ +UTILS/PLOTFRACTALITY.m
@@ -71,17 +71,17 @@ dg_dpp = cat(1, parsdata.dg_dpp{kk});
 npp = cat(1, parsdata.npp{kk});
 
 %%% Data fits
-% set(gca, 'XScale', 'log')
-% set(gca, 'YScale', 'log') % Logarithmic axes scales
+set(gca, 'XScale', 'log')
+set(gca, 'YScale', 'log') % Logarithmic axes scales
 
 % Instantaneous (Logarithmic, Linear regression, Without intercept)
 % fit1 = fit(dg_dpp, npp, 'power1'); % fitting y = a*x^b
 fit1 = fitlm(table(log(dg_dpp), log(npp)), 'linear');
 % df1 = fit1.b;
 % kf1 = fit1.a;
-df1 = [fit1.Coefficients.Estimate(2), lrfit.Coefficients.SE(2)];
+df1 = [fit1.Coefficients.Estimate(2), fit1.Coefficients.SE(2)];
 kf1 = [exp(fit1.Coefficients.Estimate(1)),...
-    lrfit.Coefficients.SE(1) * exp(lrfit.Coefficients.Estimate(1))];
+    fit1.Coefficients.SE(1) * exp(fit1.Coefficients.Estimate(1))];
 xfit = (min(dg_dpp) : range(dg_dpp) / (10 * numel(dg_dpp)) : max(dg_dpp))';
 y_fit1 = [kf1(1) .* xfit .^ df1(1),... % the main fit
     (kf1(1) + kf1(2)) .* xfit .^ (df1(1) + df1(2)),... % upper bound
@@ -92,8 +92,10 @@ anotxt1 = "d_f = " + num2str(df1(1), '%.2f') + " +/- " +...
     ", k_f = " + num2str(kf1(1), '%.2f') + " +/- " +...
     num2str(kf1(2), '%.2f');
 cl1 = [0.4660 0.6740 0.1880];
-plot(xfit, y_fit1, 'Color', cl1, 'LineStyle', '--', 'LineWidth', 2.5);
-annotation('textbox', [0.77 0.15 0.5 0.1], 'String', anotxt1,...
+plot(xfit, y_fit1(:,1), 'Color', cl1, 'LineWidth', 2.5);
+plot(xfit, y_fit1(:,2), xfit, y_fit1(:,3), 'LineStyle', '--',...
+    'Color', cl1, 'LineWidth', 1.5);
+annotation('textbox', [0.75 0.2 1 0.1], 'String', anotxt1,...
     'Color', cl1, 'EdgeColor', cl1, 'FitBoxToText', 'on',...
     'FontName', 'Times New Roman', 'FontSize', 14, 'FontWeight', 'bold');
 
@@ -107,12 +109,12 @@ anotxt0 = "d_f_,_0 = " + num2str(df0, '%.2f') +...
 cl0 = [0.5 0.5 0.5];
 plot(xfit, yfit0, 'Color', cl0, 'LineStyle', '-.', 'LineWidth', 1.5);
     % Plotting the fit
-annotation('textbox', [0.77 0.1 0.5 0.1], 'String', anotxt0,...
+annotation('textbox', [0.75 0.1 1 0.1], 'String', anotxt0,...
     'Color', cl0, 'EdgeColor', cl0, 'FitBoxToText', 'on',...
     'FontName', 'Times New Roman', 'FontSize', 14); % Fractal properties...
         % ...annotated
 
-legtxt = [legtxt; 'Linear regression'; 'Sorensen (2011)'];
+legtxt = [legtxt; 'Linear regression'; ''; ''; 'Sorensen (2011)'];
 
 title('Time- & population-averaged fractal properties',...
     'FontName', 'Times New Roman', 'FontSize', 20, 'FontWeight', 'bold');
@@ -127,8 +129,8 @@ axis padded
 
 disp(' ')
 disp('Average fractal properties:')
-fprintf('df_ens = %.2f \n', df1)
-fprintf('kf_ens = %.2f \n', kf1)
+fprintf('df_ens = %.2f +/- %.2f\n', df1(1), df1(2))
+fprintf('kf_ens = %.2f +/- %.2f\n', kf1(1), kf1(2))
 
 if nargout < 3
     clear h_fract;  % Deleting figure handle if not requested as an output
