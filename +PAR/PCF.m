c90511960c2ac46c2a61ee317e543f314ad31e50
Hamed Nikookar <70616948+Hamed-NKR@users.noreply.github.com>
2021-11-19 09:08:35 -0800

    Update PCF

--- +PAR/PCF.m
+++ +PAR/PCF.m
@@ -1,7 +1,7 @@
 function [g, r, morph, h] = PCF(pars, kk_pars, kk_h)
-% "PCF" calculates the Pair Correlation Function (PCF) for single...
-%   ...aggregates. The relations used are based on Heinson et al. ...
-%   ...(2012)'s work.
+% "PCF" calculates the Pair Correlation Function (PCF) and fractal...
+%   ...properties for individual aggregates. The relations used are...
+%   ...based on Heinson et al. (2012)'s work.
 % ----------------------------------------------------------------------- %
 % 
 % Inputs:
@@ -11,9 +11,9 @@ function [g, r, morph, h] = PCF(pars, kk_pars, kk_h)
 % ----------------------------------------------------------------------- %
 % 
 % Outputs:
-%   g: Pair correlation function with respect to the center of mass...
-%       ...(COM) within the aggregate domain
-%   r: A discuntinuous set of distances from com within the aggregate
+%   g: Pair correlation function within the aggregate domain...
+%       ...with respect to its center of mass (COM) 
+%   r: A discrete set of distances from COM for PCF calculations
 %   morph: A data structure containing the fratcal properties of the...
 %       ...individual aggregates
 %   h: Output figure handle
@@ -26,16 +26,18 @@ else
     n_tot = length(pars.n);
 end
 
+% Assigning the aggregates to be analyzed if not given
 if ~exist('kk_pars', 'var') || isempty(kk_pars)
     kk_pars = 1 : n_tot;
 end
 
+% Initializing the aggregate indices for plotting if not defined
 if ~exist('kk_h', 'var')
     kk_h = [];
 end
 
 % Initializing the figure handle
-if isempty(k_hh)
+if isempty(kk_h)
     h = [];
 elseif length(kk_h) > 24
     error('Invalid number of output images! (should be <= 24)')
@@ -43,7 +45,7 @@ else
     hold off
     h = gcf;
 
-    % Clearing previous data (for animations)
+    % Clearing previous data
     all_axes_in_figure = findall(h, 'type', 'axes');
     n_ax = numel(all_axes_in_figure);
     for i = 1 : n_ax
@@ -53,7 +55,7 @@ else
     figure(h);
 
     % Setting figure position and background
-    h.Position = [0, 0, 2000, 892.1];
+%     h.Position = [0, 0, 2000, 892.1];
     set(h, 'color', 'white');
     
     tiledlayout('flow')
@@ -72,21 +74,21 @@ end
 
 n_agg = length(kk_pars); % Number of aggregates to be analyzed
 
-r = cell(n_agg, 1); % Initializing the discrete radial distance from COM
-g = cell(n_agg, 1); % The corresponding discretized PCF
+r = cell(n_agg, 1); % Initializing the discrete radial distance
+g = cell(n_agg, 1); % ~ the corresponding discretized PCF
 
-% Center of mass of each aggregate
+% Center (of mass) of each aggregate
 if ~exist('pars.r', 'var') || isempty(pars.r)
-    r_com = PAR.COM(pp, n); 
+    r_c = PAR.COM(pp, n); 
 else
     if isa(pars, 'AGG')
-        r_com = cat(1, pars(kk_pars).r);
+        r_c = cat(1, pars(kk_pars).r);
     else
-        r_com = cat(1, pars.r(kk_pars));
+        r_c = cat(1, pars.r(kk_pars));
     end
 end
-r_com = repelem(r_com, n, 1);
-r_com = mat2cell(r_com, n);
+r_c = repelem(r_c, n, 1);
+r_c = mat2cell(r_c, n);
 
 % Mean Primary particle size within each aggregate
 if ~exist('pars.dpp', 'var') || isempty(pars.dpp)
@@ -107,62 +109,81 @@ morph = struct('df', zeros(n_agg,1), 'phi', zeros(n_agg,1),...
 for i = 1 : n_agg
     
     r{i} = 0 : dmax(i) / (10 * n(i)) : dmax(i);
-    dr_pp = sqrt(sum((pp{i}(:, 3:5) - r_com{i}).^2, 2)) - pp{i}(:, 2) / 2;
-        % Nearest distance of each primary particle from COM
+    dr_pp0 = sqrt(sum((pp{i}(:, 3:5) - r_c{i}).^2, 2)); % Central...
+        % ...distance of each primary particle from COM
+    j0 = dr_pp0 == min(dr_pp0); % Identifying the nearest primary to COM
+    dr_pp = sqrt(sum((pp{i}(:, 3:5) -...
+        repmat(pp{i}(j0, 3:5), n(i), 1)).^2, 2)) - pp{i}(:, 2) / 2;
+        % Nearest distance of each primary from the center of the...
+            % ...nearest primary to COM!!
+        
     g{i} = zeros(1, length(r{i}));
+%     g{i}(1) = Inf; % Zero in the denominator at the center while the...
+%         % ...numerator is 1
     
     for j = 2 : numel(r{i})
         g{i}(j) = nnz(dr_pp < r{i}(j)) / (4 * pi * r{i}(j)^2 *...
-            (r{i}(j) - r{i}(j))); % Number of primaries closer than a...
-            % ...certain distance over the swept volume
+            (r{i}(j) - r{i}(j-1))); % Number of primaries closer than...
+            % ...the marched distance over the swept volume
     end
+    
+    % Removing the central point
+    r{i} = r{i}(2:end);
+    g{i} = g{i}(2:end);
     
 %     [g{i}, iu] = unique(g{i}, 'stable'); % Removing repeated elements
 %     r{i} = r{i}(iu);
     
 %     rdata = r{i}; % Storing r to be used in the fitting function
-    pcfit = @(m, rdata) m(1) * (rdata^m(2)) * exp(m(3) * rdata^m(4));
+    pcfit = @(m, rr) m(1) * (rr.^m(2)) .* exp(m(3) * rr.^m(4));
         % The theoretical PCF function to be fitted (see Heinson...
             % ...et al. (2012))
-    dr0 = [0, dmax(i)]; % Range of fit
-    m = lsqcurvefit(pcfit, dr0, r{i}, g{i}); % Least-square nonlinear...
+    m0 = [30, -5, -5, 10]; % Initial guess
+    m = lsqcurvefit(pcfit, m0, r{i}, g{i}); % Least-square nonlinear...
         % ...fitter for PCF 
     
     % Fractal properties
     morph.df(i) = m(2) + 3; % Fractal dimension
     morph.gamma(i) = m(4); % Stretching exponent
-    morph.zeta(i) = (-m(2))^(-1/m(4)); % Stretching prefactor
+    morph.zeta(i) = (-m(3))^(-1/m(4)); % Stretching prefactor
     morph.df(i) = m(1) * 4 * pi * (dpp(i)^(m(2) + 3)) / (m(2) + 3);
         % Packing factor
     
     if ismember(kk_pars(i), kk_h)
         nexttile
         
-        % PCF vs. radial distance from COM data
+        % PCF vs. radial distance from the central primary particle
         scatter(r{i}, g{i}, 25, [0.8500 0.3250 0.0980], 'filled');
         hold on
         
         % Least square fit
-        r_discrete = linspace(r{i}(1), r{i}(end), 1000);
-        plot(xfit, pcfit(m, r_discrete), 'Color', [0 0.4470 0.7410],...
-            'LineWidth', 2.5);
+        r_discrete = linspace(r{i}(1), r{i}(end));
+        plot(r_discrete, pcfit(m, r_discrete), 'Color',...
+            [0 0.4470 0.7410], 'LineWidth', 2);
+        
+         % Set axis scales to be logarithmic
+        set(gca, 'XScale', 'log')
+        set(gca, 'YScale', 'log')
         
         % Plot specs
-        title('Aggregate id: ' + num2str(kk_pars(i), '%d'),...
+        title("Agg id: " + num2str(kk_pars(i), "%d"),...
             'FontName', 'Times New Roman', 'FontWeight', 'bold');
         xlabel('r (m)', 'FontName', 'Times New Roman',...
             'FontWeight', 'bold')
-        ylabel('g(r) (m^-3)', 'FontName', 'Times New Roman',...
+        ylabel('g(r) (m^-^3)', 'FontName', 'Times New Roman',...
             'FontWeight', 'bold')
         set(gca, 'FontName', 'Times New Roman')
-        legend(['PCF data'; 'LS fit'], 'Location', 'northwest',...
+        legend({'PCF data', 'LS fit'}, 'Location', 'northwest',...
             'FontName', 'Times New Roman')
-        annotation('textbox', 'String', 'd_f = ' +...
-            num2str(morph.df(i), '%.2f') + '\phi = ' +...
-            num2str(morph.phi(i), '%.2f') + '\gamma = ' +...
-            num2str(morph.gamma(i), '%.2f') + '\zeta = ' +...
-            num2str(morph.zeta(i), '%.2f'), 'FitBoxToText', 'on',...
-            'FontName', 'Times New Roman'); % Fractal properties annotated
+%         anotxt =  "n_pp = " + num2str(n(i), '%d') + newline +...
+%             "d_pp = " + num2str(dpp(i,1), '%1.1e') + " (m)" + newline +...
+%             "d_f = " + num2str(morph.df(i), '%.2f') + newline +...
+%             "\phi = " + num2str(morph.phi(i), '%.2f') + newline +...
+%             "\gamma = " + num2str(morph.gamma(i), '%.2f') + newline +...
+%             "\zeta = " + num2str(morph.zeta(i), '%.2f');
+%         annotation('textbox', 'String', anotxt, 'FitBoxToText', 'on',...
+%             'FontName', 'Times New Roman');
+%             % Fractal properties annotated
         axis padded
     end
     
