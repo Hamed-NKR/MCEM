b2f943690555ff7e1ef745aa2bd7ed2defe79a39
Hamed Nikookar <70616948+Hamed-NKR@users.noreply.github.com>
2021-11-25 20:41:11 -0800

    Update 2 PROJECTION
    Improving the visualization, Minor bug fixes

--- +PAR/PROJECTION.m
+++ +PAR/PROJECTION.m
@@ -1,21 +1,24 @@
-function [pa_avg, h] = PROJECTION(pars, rsl_samp, rsl_avg, kk_pars, kk_h)
+function [pa_avg, af_avg, h] = PROJECTION(pars, kk_pars, rsl_samp,...
+    rsl_avg, kk_h)
 % "PROJECTION" computes the orientation averaged projected area of the...
 %   ...agggregates based on Monte Carlo (MC) approach.
 % ----------------------------------------------------------------------- %
 % 
 % Inputs:
 %   pars: Particle information structure/class
+%   kk_pars: Indices of the aggregates to be analyzed
 %   rsl_samp: Sampling resolution (number of spatial points used by MC)...
 %       ...at each view angle for each aggregate
 %   rsl_avg: Number of random orientations used for averaging the area...
 %       ...for each aggregate in MC framework
-%   kk_pars: Indices of the aggregates to be analyzed
 %   kk_h: ~ to be plotted
 % ----------------------------------------------------------------------- %
 %
 % Outputs:
 %   pa_avg: Average projected area array for each aggregate over diffeerent
 %       ...angles
+%   af_avg: Fraction of orientation averaged projected area to the...
+%       ...surrounding rectangle
 %   h: Output figure handle (binary response plots of the samples points...
 %       ...being inside(1)/outside(0) the aggregate layed over the...
 %       ...original aggregate outline)
@@ -28,26 +31,26 @@ else
     n_tot = length(pars.n);
 end
 
+% Assigning the aggregates to be analyzed if not given
+if ~exist('kk_pars', 'var') || isempty(kk_pars)
+    kk_pars = 1 : n_tot;
+end
+
 % Determining the sampling resolution if missing
 if ~exist('rsl_samp', 'var') || isempty(rsl_samp)
-    rsl_samp = 1e6;
+    rsl_samp = 1e4;
 elseif rsl_samp < 1e2
     error('Spatial resolution too low! (better be >= 100)')
 end
 
 % Determining the averaging resolution if missing
 if ~exist('rsl_avg', 'var') || isempty(rsl_avg)
-    rsl_avg = 1e2;
+    rsl_avg = 20;
 elseif rsl_avg < 10
     error('Angular resolution too low! (better be >= 10)')
 end
 jj = round(1 + (rsl_avg - 1) .* (0 : 1/3 : 1)'); % Indices of angles to...
 % ...be plotted
-
-% Assigning the aggregates to be analyzed if not given
-if ~exist('kk_pars', 'var') || isempty(kk_pars)
-    kk_pars = 1 : n_tot;
-end
 
 % Initializing the aggregate indices for plotting if not defined
 if ~exist('kk_h', 'var')
@@ -72,11 +75,14 @@ else
 
     figure(h);
 
-    % Setting figure position and background
-%     h.Position = [0, 0, 2000, 892.1];
+    % Setting figure size, position and background
+    h.Position = [0, 0, 892.1 * length(kk_h) / 4, 892.1];
     set(h, 'color', 'white');
     
-    tiledlayout(length(kk_h),4);
+    tt = tiledlayout(4, length(kk_h));
+    tt.TileSpacing = 'compact';
+    tt.Padding = 'compact';
+    tt.TileIndexing = 'columnmajor';
 end
 
 % Compiling the aggregates' properties
@@ -89,21 +95,31 @@ else
 end
 
 n_agg = length(kk_pars); % Number of aggregates to be analyzed
+pa = zeros(n_agg, length(rsl_avg), 2); % Initializing the projected area...
+    % ...set (average areas & area fractions)
 
-pa = zeros(n_agg, length(rsl_avg)); % Initializing the projected area set
+disp('Computing:');
+UTILS.TEXTBAR([0, n_agg]); % Initializing 1st layer textbar (for aggergates)
 
 for i = 1 : n_agg
+    fprintf('Aggregate %d:', i);
+    disp(newline)
     
     angs = 2 * pi * rand(rsl_avg, 3); % A uniform random set of three...
         % ...intrinsic Euler angles for rotating the aggregtes to...
         % ...average the projected area
     
+    UTILS.TEXTBAR([0, rsl_avg]); % Initializing 2nd layer textbar...
+        % ...(for orientations)
+    
     for j = 1 : rsl_avg
         pp = cell2mat(PAR.ROTATE(pp0(i), n(i), angs(j,:))); % Rotating...
             % ...the aggregate in random direction and converting to matrix
         
-        x_rng = [min(pp(:,3)), max(pp(:,3))]; % Extension range in x dir.
-        y_rng = [min(pp(:,4)), max(pp(:,4))]; % ~ y dir.
+        % Extension range in x & y directions
+        rpmax = max(pp(:,2)) / 2;
+        x_rng = [min(pp(:,3)) - rpmax, max(pp(:,3)) + rpmax];
+        y_rng = [min(pp(:,4)) - rpmax, max(pp(:,4)) + rpmax];
         
         pp_temp = repmat(pp, rsl_samp, 1);
         
@@ -112,7 +128,7 @@ for i = 1 : n_agg
                 % ...MC uniformly random points 
         
         mcstat = sqrt(sum((pp_temp(:, 3:4) -...
-            repelem(mcpoints, n(i), 1)).^2, 2)) <= pp_temp(:, 2);
+            repelem(mcpoints, n(i), 1)).^2, 2)) <= pp_temp(:, 2) / 2;
                 % Check if the MC points fall within primaries
         mcstat = mat2cell(mcstat, n(i) * ones(rsl_samp,1));
         
@@ -122,8 +138,12 @@ for i = 1 : n_agg
         end
         mcstat = cell2mat(mcstat);
         
-        pa(i,j) = nnz(mcstat) / length(mcpoints); % Fraction of points...
+        pa(i,j,2) = nnz(mcstat) / length(mcpoints); % Fraction of points...
             % ...falling inside the domain
+        
+        pa(i,j,1) = abs(pa(i,j,2) * (x_rng(2) - x_rng(1)) *...
+            (y_rng(2) - y_rng(1))); % Multiplying by the total domain...
+                % ...area to get the projected area
         
         % Plotting the results
         if ismember(kk_pars(i), kk_h) && ismember(j, jj) % Plotting only...
@@ -131,38 +151,59 @@ for i = 1 : n_agg
             nexttile
 
             % The original outline of aggregate (i)
-            viscircles([pp(:,3), pp(:,4)], pp(:,2) ./ 2,...
+            pl1 = viscircles([pp(:,3), pp(:,4)], pp(:,2) / 2,...
                 'EnhanceVisibility', false, 'Color', [0 0.4470 0.7410],...
-                'LineWidth', 1.5); 
+                'LineWidth', 1);
+            pl1.Children.Color(4) = 0.6;
             hold on
                         
             % MC points
-            scatter(mcpoints(mcstat,1), mcpoints(mcstat,2), 1,...
+            pl2 = scatter(mcpoints(mcstat,1), mcpoints(mcstat,2), 1,...
                 [0.8500 0.3250 0.0980], 'filled');
+            
             % Plot specs
-            title("Agg id: " + num2str(kk_pars(i), "%d"),...
-                'FontName', 'Times New Roman', 'FontWeight', 'bold');
-            xlabel('x (m)', 'FontName', 'Times New Roman',...
-                'FontWeight', 'bold')
-            ylabel('y(m)', 'FontName', 'Times New Roman',...
-                'FontWeight', 'bold')
+            axis equal
+            box on
+            
             xlim(x_rng)
             ylim(y_rng)
-            set(gca, 'FontName', 'Times New Roman')
+            set(gca, 'FontName', 'SansSerif', 'FontSize', 11,...
+                'TickLength', [0.04 0.04])
+            
+            if j == min(jj)
+                title({"Agg id: " + num2str(kk_pars(i), "%d"), ""},...
+                    'FontName', 'SansSerif', 'FontWeight', 'bold',...
+                    'FontSize', 14);
+            end
+            
             if find(kk_h == kk_pars(i)) == 1 &&...
                     find(jj == j) == 1 % Only one legend for the whole plot
-                lgd = legend({'Agg outline', 'MC points'},...
-                'FontName', 'Times New Roman');
-                lgd.Layout.Tile = 'south';
+                lgd = legend([pl1, pl2],...
+                {'Agg outline', 'MC points'},'Orientation','horizontal',...
+                'FontName', 'SansSerif', 'FontSize', 12);
+                lgd.Layout.Tile = 'north';
             end
-            axis padded
+            
+            UTILS.TEXTBAR([j, rsl_avg]); % Completing orientation j
         end
     end
-end        
+    disp(newline)
+    UTILS.TEXTBAR([i, n_agg]); % Finishing j
+    disp(newline)
+end
 
-pa_avg = mean(pa,2); % Averaging over orientations
+title(tt, 'Aggregate projected area in different orientations',...
+    'FontName', 'SansSerif', 'FontWeight', 'bold', 'FontSize', 18)
+subtitle(tt, ' ','FontName', 'SansSerif', 'FontSize', 8)
+xlabel(tt, {'\fontsize{4} ', '\fontsize{14}x (m)'},'interpreter','tex',...
+    'FontName', 'SansSerif', 'FontWeight', 'bold')
+ylabel(tt, {'\fontsize{14}y (m)', '\fontsize{4} '},'interpreter','tex',...
+    'FontName', 'SansSerif', 'FontWeight', 'bold')
 
-if nargout < 2
+pa_avg = mean(pa(:,:,1), 2); % Averaging are over orientations
+af_avg = mean(pa(:,:,2), 2); % Averaged area fraction
+
+if nargout < 3
     clear h; % Deleting figure handle if not requested as an output
 end
 
